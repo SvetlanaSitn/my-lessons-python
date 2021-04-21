993220546c9f101626d4252b7bf1f4a40beb1cd4
SvetlanaSitn <46673852+SvetlanaSitn@users.noreply.github.com>
2021-04-21 18:58:04 +0300

    Add files via upload

---
+++ lesson-1/main.py
@@ -0,0 +1,28 @@
+# Переменная - это ячейка в памяти. Переменные можем запрашивать от пользователя
+# или сами устанавливать. Переменные позволяют хранить информацию и использовать ее в дальнейшем.
+
+# Типы переменных в Python не обьявляются очевидно, но они присутствуют.
+# name_1 = 'Hello World!'
+# name_2 = 100
+# word = "м'яч"
+#
+# print(word)
+#
+# name = input("Введите Ваше имя: ")
+# surname = input("Please, enter your surname: ")
+# age = input("Please, enter your age: ")
+# print("Ваше имя и фамилия: ", name, surname)
+# print("Возраст: ", age)
+#
+# city = input("укажите свой город: ")
+# print("city: ", city)
+
+
+#
+# print("Enter your city: ")
+# your_locate=input()
+# print("Your city is ", your_locate)
+#
+# name = input("Please, enter your name: ")
+# surname = input("Please, enter your surname: ")
+# print("Name: {1}, surname: {0}.". format(name, surname))
